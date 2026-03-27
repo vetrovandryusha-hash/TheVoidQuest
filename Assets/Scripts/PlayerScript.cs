107a8d0ff7b3f6bdf5f08d879819fa4860857148
vetrovandryusha-hash <vetrov.andryusha@gmail.com>
2026-03-27 16:06:46 +0100

    Speed

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,6 +9,8 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] float speed = 5f;
     [SerializeField] private float mouseSensivity;
     [SerializeField] private GameObject cameraHolder;
+    [SerializeField]private float stamina = 100;
+    [SerializeField] private bool canRun = true;
     private float verticalMoveRotation;
     void Start()
     {
@@ -17,11 +19,21 @@ public class PlayerScript : MonoBehaviour
     }
     private void Update()
     {
+       
         LookCamera();
     }
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetKey(KeyCode.LeftShift) && canRun)
+        {
+            speed = 10f;
+            StartCoroutine("minusStamina");
+        }
+        else
+        {
+            speed = 5f;
+        }
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
@@ -34,5 +46,17 @@ public class PlayerScript : MonoBehaviour
         verticalMoveRotation += Input.GetAxisRaw("Mouse Y") * mouseSensivity;
         verticalMoveRotation = Mathf.Clamp(verticalMoveRotation, -75f, 75f);
         cameraHolder.transform.localEulerAngles = Vector3.left * verticalMoveRotation;
+    }
+    IEnumerator minusStamina()
+    {
+        if(stamina > 0)
+        {
+            stamina--;
+            yield return new WaitForSeconds(0.2f);
+        }
+        else
+        {
+            canRun = false;
+        }
     }
 }
