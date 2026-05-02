5563b1cfcd3f9c760e86f92fb3091da75f926237
Sepiafair <osheykovlad@gmail.com>
2026-05-02 14:00:10 +0300

    AttackTimer for dark rooms (need some fixes))

--- Assets/Scripts/DarkRoomManager.cs
+++ Assets/Scripts/DarkRoomManager.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DarkRoomManager : MonoBehaviour
 {
     [SerializeField] private float timer = 5f;
+    private GameObject screamerPanel;
     IEnumerator AttackTimer()
     {
         yield return new WaitForSeconds(timer);
+        //screamerPanel.SetActive(true);
+        screamerPanel = GameObject.Find("ScreamerPanel");
+        screamerPanel.GetComponent<Image>().color = new Color(0, 0, 0, 255f);
+        screamerPanel.transform.GetChild(0).gameObject.SetActive(true);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Timer started.");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine("AttackTimer");
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Timer stopped.");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StopCoroutine("AttackTimer");
+        }
     }
 }
